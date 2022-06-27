b94a4ab4b8fb0de226d2aaecf13820e53f58275d
Loc <84399418+boyanh155@users.noreply.github.com>
2022-06-27 14:48:21 +0700

    Update

--- src/components/Shop/ContactUs.js
+++ src/components/Shop/ContactUs.js
@@ -1,4 +1,5 @@
-import React, { Suspense, useState } from "react";
+import React, { Suspense, useState,useRef } from "react";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../../assets/css/shop/contact.css";
 import {
   Input,
@@ -13,7 +14,12 @@ import {
 } from "reactstrap";
 // pic
 const ContactUs = () => {
+  // form e
+  const formE = useRef(null)
+  // state
+  //email,invoice,message,issue
   const [input, setInput] = useState({});
+  // email
   const [check, setCheck] = useState();
   //email validation
   const validateEmail = (e) => {
@@ -28,21 +34,29 @@ const ContactUs = () => {
       setCheck(undefined);
     }
   };
+  // Handle submit
+  const submitHandler = async ()=>{
+    console.log("Submit Contact")
+  }
   return (
     <Suspense className="CWrapper" fallback={<h1>Loading...</h1>}>
       <div className="CWrapper">
       <div className="centerWord">Contact Us</div>
       <div className="ContactWrapper">
-        <Form action="/contact" method="POST">
-          <Row className="ContactWrapper mx-4 justify-content-center">
-            <Col className="ContactWrapper__left relative-7" md={5}>
-              {/* email box */}
+        <Form ref={formE} inline action="/contact" method="POST" onSubmit={submitHandler}>
+          <Row className="ContactWrapper mx-4 justify-content-space-between">
+            {/* Col left */}
+            <Col className="ContactWrapper__left relative-7" md={4}>
+              {/* Row 1 - email box */}
+              
+              <Row>
               <FormGroup floating>
                 <Input
                   type="email"
                   id="emailInput"
                   placeholder="Enter your email"
                   name="email"
+                  value={input.email}
                   onInput={(e) => validateEmail(e)}
                   onChange={(e) =>
                     setInput({
@@ -50,24 +64,144 @@ const ContactUs = () => {
                       [e.target.name]: e.target.value,
                     })
                   }
+                  required
                   valid={check === true}
                   invalid={check === false}
                 />
                 <Label style={{ color: "#ccc" }} htmlFor="emailInput">
                   example@gmail.com
                 </Label>
-                <FormFeedback invalid>Incorrect email</FormFeedback>
-                <FormFeedback valid>That look good</FormFeedback>
+                {/* <FormFeedback invalid>Incorrect email</FormFeedback> */}
+                {/* <FormFeedback valid>That look good</FormFeedback> */}
                 <FormText>Email</FormText>
               </FormGroup>
-              {/* invoice box */}
+              </Row>
+                {/*  */}
+              {/* Row 2 - invoice box */}
+              <Row>
+                <FormGroup floating>
+                  <Input
+                  type="text"
+                  id="invoiceInput"
+                  placeholder="Enter invoice ID"
+                  name="invoice"
+                  value={input.invoice}
+
+                  onInput={e=>{
+                    setInput({
+                      ...input,
+                      [e.target.name]:e.target.value
+                    }
+                    )
+                  }}
+                  >
+                  
+                  </Input>
+                  <Label style={{ color: "#ccc" }} htmlFor="invoiceInput">
+                  example@gmail.com
+                </Label>
+                <FormFeedback invalid>Incorrect ID</FormFeedback>
+                <FormFeedback valid>Nice!</FormFeedback>
+                <FormText>Invoice ID</FormText>
+
+                </FormGroup>
+              </Row>
             </Col>
-            <Col className="ContactWrapper__right">{/* message box */}</Col>
+              {/* Col middle */}
+              <Col className="ContactWrapper__right" md={4}>
+              {/* message box */}
+            <FormGroup floating>
+              <Input
+              id="messageInput"
+              name="message"
+              value={input.message}
+              type="textarea"
+              style={{height:"156px",resize:"none"}}
+              onInput={e=>{
+                setInput({
+                  ...input,
+                  [e.target.name]:e.target.value
+                }
+                )
+              }}
+              >
+              </Input>
+              <Label
+              style={{ color: "#ccc"}}
+              htmlFor="messageInput"
+              >
+                Enter your message
+              </Label>
+              <FormText>Message</FormText>
+
+            </FormGroup>  
+            </Col>
+            {/* Col right */}
+            <Col className="ContactWrapper__middle" md={4}>
+              {/* Select issue */}
+              {/* Row 1 - select issue */}
+              <Row>
+
+              <FormGroup>
+                <Input
+                id="issueInput"
+                bsSize="lg"
+                name="issue"
+                type="select"
+                multiple
+                onInput={e=>{
+                  setInput({
+                    ...input,
+                    [e.target.name]:e.target.value
+                  }
+                  )
+                }}
+                style={{height:"92px"}}
+                >
+                  <option selected="selected">Phản hồi dịch vụ tệ</option>
+                  <option>Phản hồi trang web tệ </option>
+                  <option>Góp ý về hoạt động</option>
+                  <option>Liên lạc công việc</option>
+                  <option>Tìm thông tin của cửa hàng</option>
+                  <option>Đánh giá sản phẩm</option>
+                  <option>Khác...</option>
+                </Input>
+                  <FormText>
+                    Issue
+                  </FormText>
+              </FormGroup>
+              </Row>
+              {/* Row 2 - button */}
+                  <Row className="buttonWrap" >
+                    <Col md={2}>
+                      <Button color="primary" >
+                        Send
+                      </Button>
+                    </Col>
+                    <Col md={2}>
+                      <Button color="primary" 
+                      onClick={e=>{
+                        e.preventDefault()
+                          //email,invoice,message,issue
+                        setInput({  
+                          email:"",
+                          invoice:"",
+                          message:"",
+                        })
+                        // formE.current.reset()
+                      }}
+                      >
+                        Reset
+                      </Button>
+                    </Col>
+                  </Row>
+
+            </Col>
+
           </Row>
         </Form>
       </div>
       </div>
-     
     </Suspense>
   );
 };
