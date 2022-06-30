efeb9d28a7ff69fb5328f4985ae9f3d70e359cc7
Loc <84399418+boyanh155@users.noreply.github.com>
2022-06-30 18:38:24 +0700

    Proccess fotter

--- src/components/Shop/Footer/Info.js
+++ src/components/Shop/Footer/Info.js
@@ -3,13 +3,78 @@ import React from 'react'
 const Info = () => {
   return (
     
-    <div>
-        {/* R1 - C1 - Help */}
-        {/* R1 - C2 - Company */}
-        {/* R1 - C3 - CService */}
-        {/* R1 - C4 - Social */}
-        {/* R1 - C5 - App */}
-        Info
+    <div className='row no-gutters Info__Row'>
+      {/* R1 - C1 - Help */}
+      <div className="col Info__Col">
+        <div className="Info__Col--title">
+        Help & support
+
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          shipping info
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          returns
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          how to order
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          how to track
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          size guide
+        </div>
+      </div>
+      {/* R1 - C2 - Company */}
+      <div className="col Info__Col">
+        <div className="Info__Col--title">
+        Company info
+
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          our company
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          our team
+        </div>
+      </div>
+      {/* R1 - C3 - CService */}
+      <div className="col Info__Col">
+        <div className="Info__Col--title">
+        Customer care
+
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          contact us
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          payment method
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          offer redemption
+        </div>
+      </div>
+      {/* R1 - C4 - Social */}
+      <div className="col Info__Col">
+        <div className="Info__Col--title">
+        Find us on
+
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          
+        </div>
+      </div>
+      {/* R1 - C5 - App */}
+      <div className="col Info__Col">
+        <div className="Info__Col--title">
+        App
+
+        </div>
+        <div className="Info__Col--content row no-gutters">
+          
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Shop/ShopFooter.js
+++ src/components/Shop/ShopFooter.js
@@ -5,13 +5,22 @@ const Subscribe = React.lazy(()=>import("./Footer/Subscribe"))
 const Policy = React.lazy(()=>import("./Footer/Policy"))
 const ShopFooter = () => {
   return (
-    <div className="Wrapper">
-        {/* R1 - Info */}
-        <Info/>
-        {/* R2 - Subscribe */}
-        <Subscribe/>
-        {/* R3 - Policy */}
-        <Policy/>
+    <div className="SF_Wrapper grid">
+      <div className="SF_Content">
+        <div className="SF_Content__Info">
+          {/* R1 - Info */}
+          <Info/>
+        </div>
+        <div className="SF_Content__Subscribe">
+          {/* R2 - Subscribe */}
+          <Subscribe/></div>
+        <div className="SF_Content__Policy">
+          {/* R3 - Policy */}
+          <Policy/>
+        </div>
+
+      </div>
+
     </div>
   )
 }
